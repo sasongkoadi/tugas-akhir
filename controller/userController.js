b03750ef087aa24d870186b60f6b51e64367ac39
sasongkoadi <sasongko005@gmail.com>
2018-02-02 01:28:19 +0800

    add controller method data

--- controller/userController.js
+++ controller/userController.js
@@ -32,7 +32,38 @@ const getUserById = (request, response) => {
   })
 }
 
+const addUser = (request, response) => {
+  const namaDepan = request.body.namaDepan
+  const namaBelakang = request.body.namaBelakang
+  const alamatUser = request.body.alamatUser
+  const emailUser = request.body.emailUser
+  const nomerPonselUser = request.body.nomerPonselUser
+  const userName = request.body.userName
+  const data = {
+    namaDepan,
+    namaBelakang,
+    alamatUser,
+    emailUser,
+    nomerPonselUser,
+    userName
+  }
+  userModel.addUser(data, (error, result) => {
+    if (error) {
+      response.status(400)
+      response.json({
+        message: error.message,
+        stack: error.stack
+      })
+    } else {
+      response.json({
+        user: result
+      })
+    }
+  })
+}
+
 module.exports = {
   getAllUser,
-  getUserById
+  getUserById,
+  addUser
 }
