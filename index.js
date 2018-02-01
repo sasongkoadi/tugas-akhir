07082febc64219a91766a3f03c3e0f62625e31d5
sasongkoadi <sasongko005@gmail.com>
2018-02-01 15:02:16 +0800

    add function to connect server

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(jsonParser)
 app.use(router)
 
 // enable CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+app.use(function (request, response, next) {
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
