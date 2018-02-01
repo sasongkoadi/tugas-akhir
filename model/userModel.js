44b20da2e10dd85b541dafd3a69b1a0bf86d7efe
sasongkoadi <sasongko005@gmail.com>
2018-02-02 01:28:00 +0800

    add userdata method

--- model/userModel.js
+++ model/userModel.js
@@ -19,16 +19,15 @@ const getUserById = (id, callback) => {
   })
 }
 
-const addUser = ({namaDepan, namaBelakang, alamatUser, emailUser, nomerPonselUser, userName}, callback)  => {
+const addUser = ({namaDepan, namaBelakang, alamatUser, emailUser, nomerPonselUser, userName}, callback) => {
   conn.query({
     sql: 'insert into User (namaDepan, namaBelakang, alamatUser, emailUser, nomerPonselUser, userName) values (?, ?, ?, ?, ?, ?)',
-    timeout: 4000,
     values: [namaDepan, namaBelakang, alamatUser, emailUser, nomerPonselUser, userName]
   }, (error, result) => {
     if (error) {
       callback(error, null)
     } else {
-      getUserById(result.inserId, null)
+      getUserById(result.insertId, callback)
     }
   })
 }
