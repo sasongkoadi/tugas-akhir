06b581a7a594d2accc9de896ec47b99c79689588
sasongkoadi <sasongko005@gmail.com>
2018-02-02 19:34:37 +0800

    add api address

--- router/routes.js
+++ router/routes.js
@@ -5,10 +5,12 @@ const pemilikMotor = require('../controller/pemilikMotorController')
 
 // User Controller
 router.get('/users', userController.getAllUser)
-router.get('/users/:id', userController.getUserById)
+router.get('/user/:id', userController.getUserById)
 router.post('/adduser', userController.addUser)
+router.delete('/user/:id', userController.deleteUser)
 
 // Pemilik Motor Controller
 router.get('/pemilikmotor', pemilikMotor.getAllPemilikMotor)
+router.get('/pemilikmotor/:id', pemilikMotor.getPemilikMotorById)
 
 module.exports = router
