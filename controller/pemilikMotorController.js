ca087790fcd5a4665550c6607218381225099d9b
sasongkoadi <sasongko005@gmail.com>
2018-02-02 19:33:58 +0800

    add controller data when get pemilikmotor data

--- controller/pemilikMotorController.js
+++ controller/pemilikMotorController.js
@@ -16,6 +16,23 @@ function getAllPemilikMotor (req, res) {
   })
 }
 
+const getPemilikMotorById = (request, response) => {
+  const id = request.params.id
+  pemilikMotorModel.getPemilkMotorById(id, (error, result) => {
+    if (error) {
+      response.status(400).json({
+        message: error.message,
+        stack: error.stack
+      })
+    } else {
+      response.json({
+        pemilikmotor: result
+      })
+    }
+  })
+}
+
 module.exports = {
-  getAllPemilikMotor
+  getAllPemilikMotor,
+  getPemilikMotorById
 }
