1f4b1c9bb9361a5f71510c59d2a397ade8cdfd60
sasongkoadi <sasongko005@gmail.com>
2018-02-06 23:46:14 +0800

    add control motor data

--- controller/motorController.js
+++ controller/motorController.js
@@ -52,8 +52,45 @@ const createMotorData = (request, response) => {
   })
 }
 
+const updateMotorData = (request, response) => {
+  const id = request.params.id
+  const namaMotor = request.body.namaMotor
+  const jenisMotor = request.body.jenisMotor
+  const CC = request.body.CC
+  motorDataModel.updateMotorData(namaMotor, jenisMotor, CC, id, (error, result) => {
+    if (error) {
+      response.status(400).json({
+        message: error.message,
+        stack: error.stack
+      })
+    } else {
+      response.json({
+        motor: result
+      })
+    }
+  })
+}
+
+const deleteMotorData = (request, response) => {
+  const id = request.params.id
+  motorDataModel.deleteMotorData(id, (error, result) => {
+    if (error) {
+      response.status(400).json({
+        message: error.message,
+        stack: error.stack
+      })
+    } else {
+      response.json({
+        result
+      })
+    }
+  })
+}
+
 module.exports = {
   getMotorData,
   getMotorDataId,
-  createMotorData
+  createMotorData,
+  updateMotorData,
+  deleteMotorData
 }
