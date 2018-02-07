172ba12e1f9f6ae6ea27aee8f78177b335e67eee
sasongkoadi <sasongko005@gmail.com>
2018-02-07 23:57:06 +0800

    add sql syntax for get, update dan delete pemilik motor data

--- model/pemilikMotorModel.js
+++ model/pemilikMotorModel.js
@@ -8,13 +8,25 @@ const getAllPemilikMotor = (callback) => {
 }
 
 const getPemilkMotorById = (id, callback) => {
-  conn.query('select pemilikMotor.id, namaDepan, namaBelakang,username, alamatUser, nomerPonselUser, namaMotor, jenisMotor, CC, platNomer, nomerRangka, nomerMesin from pemilikMotor left join User on pemilikMotor.namaPemilik = User.id left join Motor on pemilikMotor.motorPemilik = Motor.id where User.id =' + id, (error, result) => {
+  conn.query('select pemilikMotor.id, namaDepan, namaBelakang,username, alamatUser, nomerPonselUser, namaMotor, jenisMotor, CC, platNomer, nomerRangka, nomerMesin from pemilikMotor left join User on pemilikMotor.namaPemilik = User.id left join Motor on pemilikMotor.motorPemilik = Motor.id where pemilikMotor.id =' + id, (error, result) => {
     if (error) {
       callback(error, null)
     } else {
       callback(null, result)
     }
   })
+}
+
+const getPemilikMotorByUserId = (id, callback) => {
+  conn.query('select pemilikMotor.id, namaDepan, namaBelakang,username, alamatUser, nomerPonselUser, namaMotor, jenisMotor, CC, platNomer, nomerRangka, nomerMesin from pemilikMotor left join User on pemilikMotor.namaPemilik = User.id left join Motor on pemilikMotor.motorPemilik = Motor.id where User.id = ?',
+[id],
+(error, result) => {
+  if (error) {
+    callback(error, null)
+  } else {
+    callback(null, result)
+  }
+})
 }
 
 const addPemilikMotor = (namaPemilik, motorPemilik, platNomer, nomerRangka, nomerMesin, callback) => {
@@ -29,8 +41,36 @@ const addPemilikMotor = (namaPemilik, motorPemilik, platNomer, nomerRangka, nome
 })
 }
 
+const updatePemilikMotor = (namaPemilik, motorPemilik, platNomer, nomerRangka, nomerMesin, id, callback) => {
+  conn.query('update pemilikMotor set namaPemilik = ?, motorPemilik = ?, platNomer = ?, nomerRangka = ?, nomerMesin = ? where id = ?',
+[namaPemilik, motorPemilik, platNomer, nomerRangka, nomerMesin, id], (error, result) => {
+  if (error) {
+    callback(error, null)
+  } else {
+    getPemilkMotorById(id, callback)
+  }
+})
+}
+
+const deletePemilikMotor = (id, callback) => {
+  conn.query('delete from pemilikMotor where id = ?',
+[id],
+(error, result) => {
+  if (error) {
+    callback(error, null)
+  } else {
+    callback(null, {
+      message: 'Sucessfull delete data id = ' + id
+    })
+  }
+})
+}
+
 module.exports = {
   getAllPemilikMotor,
   getPemilkMotorById,
-  addPemilikMotor
+  getPemilikMotorByUserId,
+  addPemilikMotor,
+  updatePemilikMotor,
+  deletePemilikMotor
 }
